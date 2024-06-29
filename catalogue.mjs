4af8ef585c0ccbbfea3a4bcd381187765222538b
peteryuan <ypf9319@gmail.com>
2024-06-29 17:39:21 +0800

    add script

--- catalogue.mjs
+++ catalogue.mjs
@@ -1,18 +1,25 @@
-import { catalogue } from './data.mjs'
+import { catalogue } from "./data.mjs";
 
+import { fanyiSelection } from "./fanyi.mjs";
 
+fanyiSelection();
 
 function renderCatalogue() {
-    const catalogueEle = document.querySelector('#Catalogue')
+    const catalogueEle = document.querySelector("#Catalogue");
     catalogue.forEach((item, idx) => {
-        const pEle = document.createElement('a')
-        const routePath = location.origin.includes('localhost') ? '/' : '/speak-english-like-a-native/'
-        pEle.href = `${routePath}content?id=${item.id}`
-        pEle.className = 'catalogue'
-        pEle.innerHTML = `<div style="margin-right:1rem;">Set(${idx + 1})</div><div class="english">${item.en}</div><div class="chinese">${item.zh}</div>`
-        catalogueEle.append(pEle)
-
-    })
+        const pEle = document.createElement("a");
+        const routePath = location.origin.includes("localhost")
+            ? "/"
+            : "/speak-english-like-a-native/";
+        pEle.href = `${routePath}content?id=${item.id}`;
+        pEle.className = "catalogue";
+        pEle.innerHTML = `<div style="margin-right:1rem;">Set(${
+            idx + 1
+        })</div><div class="english">${item.en}</div><div class="chinese">${
+            item.zh
+        }</div>`;
+        catalogueEle.append(pEle);
+    });
 }
 
-renderCatalogue()
+renderCatalogue();
